aaf5661187b2673c529d2734eb379c44a1b24526
manojror <manojreddyror@gmail.com>
2015-04-29 21:53:12 -0700

    modified cal-case file

--- cal-case.rb
+++ cal-case.rb
@@ -9,25 +9,20 @@ puts "1 for Addition
 4 for Division"
 puts "enter an option number"
 option = Integer(gets.chomp)
-
-
 puts "#{option} selected" 
-
 case option
 when 1	
 	sum = a + b
-	puts "sum of #{a} and #{b} is #{sum}."
+	puts "sum of #{a} and #{b} is #{sum}"
 when 2
 	Difference = a - b
-	puts " Diff of #{a} and #{b} is #{Difference}."
+	puts " Diff of #{a} and #{b} is #{Difference}"
 when 3
 	Product = a*b
-	puts "Product of #{a} and #{b} is #{Product}."
+	puts "Product of #{a} and #{b} is #{Product}"
 when 4
 	Division = a/b
-	puts "Division of #{a} and #{b} is #{Division}."
+	puts "Division of #{a} and #{b} is #{Division}"
 else
 	puts "Wrong option selected."	
-
 end
-	
