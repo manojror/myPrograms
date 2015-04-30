3a94adcc98b7dd12d424a21b94e69ec840fa46bf
manojror <manojreddyror@gmail.com>
2015-04-29 21:21:36 -0700

    modified calcase.rb

--- cal-case.rb
+++ cal-case.rb
@@ -2,7 +2,6 @@ puts "Enter value for a"
 a = Float(gets.chomp)
 puts "Enter value for b"
 b = Float(gets.chomp)
-
 puts "select an option"
 puts "1 for Addition
 2 for Substraction
@@ -17,18 +16,18 @@ puts "#{option} selected"
 case option
 when 1	
 	sum = a + b
-	puts "sum of #{a} and #{b} is #{sum}"
+	puts "sum of #{a} and #{b} is #{sum}."
 when 2
 	Difference = a - b
-	puts " Diff of #{a} and #{b} is #{Difference}"
+	puts " Diff of #{a} and #{b} is #{Difference}."
 when 3
 	Product = a*b
-	puts "Product of #{a} and #{b} is #{Product}"
+	puts "Product of #{a} and #{b} is #{Product}."
 when 4
 	Division = a/b
-	puts "Division of #{a} and #{b} is #{Division}"
+	puts "Division of #{a} and #{b} is #{Division}."
 else
-	puts "Wrong option selected"	
+	puts "Wrong option selected."	
 
 end
 	
